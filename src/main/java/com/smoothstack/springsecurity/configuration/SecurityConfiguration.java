87b8e6ac31e6b18fb11ede01caa32a8fcc5e8747
Rob Maes <robert.maes@smoothstack.com>
2021-03-16 19:32:13 -0500

    complete step 3

---
+++ src/main/java/com/smoothstack/springsecurity/configuration/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.smoothstack.springsecurity.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author Rob Maes
+ * Mar 16 2021
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth
+      .inMemoryAuthentication()
+      .withUser("admin")
+      .password(passwordEncoder().encode("adminpass"))
+      .roles("ADMIN")
+      .and()
+      .withUser("rob")
+      .password(passwordEncoder().encode("testing1234"))
+      .roles("USER");
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
